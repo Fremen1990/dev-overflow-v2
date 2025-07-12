3d8d0d0fa60b42212e447671123b6f120c131741
devthomas <thomas.dev666@gmail.com>
2025-07-13 00:35:16 +0200

    Setup Fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
     return (
-        <h1 className={`h1-bold`}>
-            Tailwind CSS v4 with Next.js 15 is ready!
-        </h1>
-    );
+        <>
+            <h1 className={`h1-bold`}>
+                Tailwind CSS v4 with Next.js 15 is ready!
+            </h1>
+        </>
+    )
 }
